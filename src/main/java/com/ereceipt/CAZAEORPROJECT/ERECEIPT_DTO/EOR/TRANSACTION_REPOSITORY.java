9952bf1133c86e3c0fd458992ba57cfabf43fa94
laLatroni <ivofspadesunique@gmail.com>
2023-10-17 12:15:38 +0800

    testing

--- src/main/java/com/ereceipt/CAZAEORPROJECT/ERECEIPT_DTO/EOR/TRANSACTION_REPOSITORY.java
+++ src/main/java/com/ereceipt/CAZAEORPROJECT/ERECEIPT_DTO/EOR/TRANSACTION_REPOSITORY.java
@@ -1,8 +1,5 @@
 package com.ereceipt.CAZAEORPROJECT.ERECEIPT_DTO.EOR;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.io.Serializable;
 
 public interface TRANSACTION_REPOSITORY extends JpaRepository<TRANSACTION, Integer> {
 }
