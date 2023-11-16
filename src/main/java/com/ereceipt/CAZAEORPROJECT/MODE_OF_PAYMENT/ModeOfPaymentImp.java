a7ce6c06b9b92b757b3648a85fbd133aafae0f61
laLatroni <ivofspadesunique@gmail.com>
2023-11-16 10:02:41 +0800

    I THOUGHT I WAS DEAD
    FIX

---
+++ src/main/java/com/ereceipt/CAZAEORPROJECT/MODE_OF_PAYMENT/ModeOfPaymentImp.java
@@ -0,0 +1,8 @@
+package com.ereceipt.CAZAEORPROJECT.MODE_OF_PAYMENT;
+
+import com.ereceipt.CAZAEORPROJECT.CUSTOMER_TABLE.EOR.CUST_RECORDS;
+
+public interface ModeOfPaymentImp {
+    ModeOfPayment  addPayment(ModeOfPayment modeOfPayment);
+
+}
